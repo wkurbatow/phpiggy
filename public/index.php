4519417f3ee60c06a82eefef8cc2fa228f1e28b3
wkurbatow <wkurbatow@gmail.com>
2023-11-23 10:12:17 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+ini_set("memory_limit","512M");
+
+$app = include __DIR__ ."/../src/App/bootstrap.php";
+
+$app->run();
+
+
---
+++ src/App/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types= 1);
+
+include __DIR__ ."/../Framework/App.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
