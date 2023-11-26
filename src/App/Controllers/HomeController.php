f55dd7e140db8f3b3fe459b1f650f4104eb4742e
wkurbatow <wkurbatow@gmail.com>
2023-11-26 06:34:16 -0800

    3

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types= 1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
-class HomeController {
-    public function home(){
+class HomeController
+{
+    public function home()
+    {
         echo "home page";
     }
-
 }
-
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types= 1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use Framework\Router;
+//use Framework\Router;
 use App\Controllers\HomeController;
 
 
